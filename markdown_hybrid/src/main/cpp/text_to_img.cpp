8a79ea889024276d6c3855e88ef9a2232360e998
peixianzhong <peixianzhong@noreply.gitcode.com>
2025-12-18 17:11:40 +0800

    c代码告警修改

--- markdown_hybrid/src/main/cpp/text_to_img.cpp
+++ markdown_hybrid/src/main/cpp/text_to_img.cpp
@@ -2,7 +2,7 @@
  * Copyright (c) Huawei Technologies Co., Ltd. 2025-2025. All rights reserved.
  */
 #include <cstdint>
-#define __OS_ohos__ 1
+#define __OS_ohos__
 
 #ifdef __OS_ohos__
 
@@ -33,26 +33,47 @@
 extern "C" {
 #endif
 
-void safe_memcpy(void *dest, uint32_t dest_size, const void *src, uint32_t src_size) {
+void safe_memcpy(void *dest, uint32_t dest_size, const void *src, uint32_t src_size)
+{
     if (dest_size < src_size) {
         throw std::runtime_error("Insufficient space in the target buffer.");
     }
     std::memcpy(dest, src, src_size);
 }
 
-void safe_memset(void *dest, uint32_t destSize, int value, uint32_t count) {
+void safe_memset(void *dest, uint32_t destSize, int value, uint32_t count)
+{
     if (count > destSize) {
         throw std::runtime_error("The number of bytes to be set exceeds the buffer size.");
     }
     std::memset(dest, value, count);
 }
 
+const int PLANES = 1;
+const int BIT_COUNT_16 = 16;
+const int BIT_COUNT_32 = 32;
+const int ZERO = 0;
+const int ONE = 1;
+const int TWO = 2;
+const int FOUR = 4;
+const int COLOR_0XF8 = 0xF8;
+const int COLOR_0XE0 = 0xE0;
+const int COLOR_0X07 = 0x07;
+const int COLOR_0X1F = 0x1F;
+const int COLOR_0X4D42 = 0x4D42;
+const int COLOR_0X00FF0000 = 0x00FF0000;
+const int COLOR_0X0000FF00 = 0x0000FF00;
+const int COLOR_0X000000FF = 0x000000FF;
+const int COLOR_0XFF000000 = 0xFF000000;
+const int COLOR_0X73524742 = 0x73524742;
+
 typedef struct {
     uint8_t *data;
     int64_t len;
 } UInt8Data;
 
-static UInt8Data toBitmapRGB565(OH_Drawing_Bitmap *bitmap) {
+static UInt8Data toBitmapRGB565(OH_Drawing_Bitmap *bitmap)
+{
     BitMapFileHeader bmfHdr; // 定义文件头
     BitMapInfoHeader bmiHdr; // 定义信息头
     RgbQuad bmiClr[3];       // 定义调色板
@@ -64,41 +85,41 @@ static UInt8Data toBitmapRGB565(OH_Drawing_Bitmap *bitmap) {
     bmiHdr.biSize = sizeof(BitMapInfoHeader);
     bmiHdr.biWidth = w;                       // 指定图像的宽度，单位是像素
     bmiHdr.biHeight = h;                      // 指定图像的高度，单位是像素
-    bmiHdr.biPlanes = 1;                      // 目标设备的级别，必须是1
-    bmiHdr.biBitCount = 16;                   // 表示用到颜色时用到的位数 16位表示高彩色图
+    bmiHdr.biPlanes = PLANES;                      // 目标设备的级别，必须是1
+    bmiHdr.biBitCount = BIT_COUNT_16;                   // 表示用到颜色时用到的位数 16位表示高彩色图
     bmiHdr.biCompression = BI_BITFIELDS;      // RGB565格式
-    bmiHdr.biSizeImage = (w + w % 2) * h * 2; // 指定实际位图所占字节数
-    bmiHdr.biXPelsPerMeter = 0;               // 水平分辨率，单位长度内的像素数
-    bmiHdr.biYPelsPerMeter = 0;               // 垂直分辨率，单位长度内的像素数
-    bmiHdr.biClrUsed = 0; // 位图实际使用的彩色表中的颜色索引数（设为0的话，则说明使用所有调色板项）
-    bmiHdr.biClrImportant = 0; // 说明对图象显示有重要影响的颜色索引的数目，0表示所有颜色都重要
+    bmiHdr.biSizeImage = (w + w % TWO) * h * TWO; // 指定实际位图所占字节数
+    bmiHdr.biXPelsPerMeter = ZERO;               // 水平分辨率，单位长度内的像素数
+    bmiHdr.biYPelsPerMeter = ZERO;               // 垂直分辨率，单位长度内的像素数
+    bmiHdr.biClrUsed = ZERO; // 位图实际使用的彩色表中的颜色索引数（设为0的话，则说明使用所有调色板项）
+    bmiHdr.biClrImportant = ZERO; // 说明对图象显示有重要影响的颜色索引的数目，0表示所有颜色都重要
 
     // RGB565格式掩码
-    bmiClr[0].rgbBlue = 0;
-    bmiClr[0].rgbGreen = 0xF8;
-    bmiClr[0].rgbRed = 0;
-    bmiClr[0].rgbReserved = 0;
+    bmiClr[0].rgbBlue = ZERO;
+    bmiClr[0].rgbGreen = COLOR_0XF8;
+    bmiClr[0].rgbRed = ZERO;
+    bmiClr[0].rgbReserved = ZERO;
 
-    bmiClr[1].rgbBlue = 0xE0;
-    bmiClr[1].rgbGreen = 0x07;
-    bmiClr[1].rgbRed = 0;
-    bmiClr[1].rgbReserved = 0;
+    bmiClr[1].rgbBlue = COLOR_0XE0;
+    bmiClr[1].rgbGreen = COLOR_0X07;
+    bmiClr[1].rgbRed = ZERO;
+    bmiClr[1].rgbReserved = ZERO;
 
-    bmiClr[2].rgbBlue = 0x1F;
-    bmiClr[2].rgbGreen = 0;
-    bmiClr[2].rgbRed = 0;
-    bmiClr[2].rgbReserved = 0;
+    bmiClr[2].rgbBlue = COLOR_0X1F;
+    bmiClr[2].rgbGreen = ZERO;
+    bmiClr[2].rgbRed = ZERO;
+    bmiClr[2].rgbReserved = ZERO;
 
-    bmfHdr.bfType = (WORD)0x4D42; // 文件类型，0x4D42也就是字符'BM'
+    bmfHdr.bfType = (WORD)COLOR_0X4D42; // 文件类型，0x4D42也就是字符'BM'
     bmfHdr.bfSize =
         (DWORD)(sizeof(BitMapFileHeader) + sizeof(BitMapInfoHeader) + sizeof(bmiClr) + bmiHdr.biSizeImage); // 文件大小
-    bmfHdr.bfReserved1 = 0; // 保留，必须为0
-    bmfHdr.bfReserved2 = 0; // 保留，必须为0
+    bmfHdr.bfReserved1 = ZERO; // 保留，必须为0
+    bmfHdr.bfReserved2 = ZERO; // 保留，必须为0
     bmfHdr.bfOffBits =
         (DWORD)(sizeof(BitMapFileHeader) + sizeof(BitMapInfoHeader) + sizeof(bmiClr)); // 实际图像数据偏移量
 
     uint8_t *data = (uint8_t *)malloc(bmfHdr.bfSize);
-    safe_memset(data, bmfHdr.bfSize, 0, bmfHdr.bfOffBits);
+    safe_memset(data, bmfHdr.bfSize, ZERO, bmfHdr.bfOffBits);
     uint8_t *start = data;
     int32_t destsz = bmfHdr.bfSize;
     safe_memcpy(start, destsz, &bmfHdr, sizeof(BitMapFileHeader));
@@ -111,9 +132,9 @@ static UInt8Data toBitmapRGB565(OH_Drawing_Bitmap *bitmap) {
     start += sizeof(bmiClr);
     destsz -= sizeof(bmiClr);
     for (int i = 0; i < h; i++) {
-        safe_memcpy(start, destsz, bitmapAddr + (w * (h - i - 1) * 2), w * 2);
-        start += (w + w % 2) * 2;
-        destsz -= (w + w % 2) * 2;
+        safe_memcpy(start, destsz, bitmapAddr + (w * (h - i - ONE) * TWO), w * TWO);
+        start += (w + w % TWO) * TWO;
+        destsz -= (w + w % TWO) * TWO;
     }
 
     UInt8Data u8data;
@@ -158,7 +179,8 @@ typedef struct {
     DWORD bV4GammaBlue;
 } BitMapV4Header;
 
-static UInt8Data toBitmapBGRA8888(OH_Drawing_Bitmap *bitmap) {
+static UInt8Data toBitmapBGRA8888(OH_Drawing_Bitmap *bitmap)
+{
     BitMapFileHeader bmfHdr;
     BitMapV4Header bmiHdr;
 
@@ -167,39 +189,39 @@ static UInt8Data toBitmapBGRA8888(OH_Drawing_Bitmap *bitmap) {
     uint8_t *bitmapAddr = (uint8_t *)OH_Drawing_BitmapGetPixels(bitmap);
 
     // 初始化BITMAPV4HEADER
-    memset(&bmiHdr, 0, sizeof(BitMapV4Header));
+    memset(&bmiHdr, ZERO, sizeof(BitMapV4Header));
     bmiHdr.bV4Size = sizeof(BitMapV4Header);
     bmiHdr.bV4Width = w;
     bmiHdr.bV4Height = h;
-    bmiHdr.bV4Planes = 1;
-    bmiHdr.bV4BitCount = 32;
+    bmiHdr.bV4Planes = PLANES;
+    bmiHdr.bV4BitCount = BIT_COUNT_32;
     bmiHdr.bV4Compression = BI_BITFIELDS; // 使用BITFIELDS表示有自定义颜色掩码
-    bmiHdr.bV4SizeImage = w * h * 4;
-    bmiHdr.bV4XPelsPerMeter = 0;
-    bmiHdr.bV4YPelsPerMeter = 0;
-    bmiHdr.bV4ClrUsed = 0;
-    bmiHdr.bV4ClrImportant = 0;
+    bmiHdr.bV4SizeImage = w * h * FOUR;
+    bmiHdr.bV4XPelsPerMeter = ZERO;
+    bmiHdr.bV4YPelsPerMeter = ZERO;
+    bmiHdr.bV4ClrUsed = ZERO;
+    bmiHdr.bV4ClrImportant = ZERO;
     
     // 设置颜色掩码 - BGRA顺序
-    bmiHdr.bV4RedMask = 0x00FF0000;   // 红色掩码
-    bmiHdr.bV4GreenMask = 0x0000FF00; // 绿色掩码
-    bmiHdr.bV4BlueMask = 0x000000FF;  // 蓝色掩码
-    bmiHdr.bV4AlphaMask = 0xFF000000; // Alpha通道掩码
+    bmiHdr.bV4RedMask = COLOR_0X00FF0000;   // 红色掩码
+    bmiHdr.bV4GreenMask = COLOR_0X0000FF00; // 绿色掩码
+    bmiHdr.bV4BlueMask = COLOR_0X000000FF;  // 蓝色掩码
+    bmiHdr.bV4AlphaMask = COLOR_0XFF000000; // Alpha通道掩码
     
     // 设置颜色空间为sRGB
-    bmiHdr.bV4CSType = 0x73524742; // 'sRGB'
+    bmiHdr.bV4CSType = COLOR_0X73524742; // 'sRGB'
 
     // 设置文件头
-    bmfHdr.bfType = (WORD)0x4D42; // "BM"
+    bmfHdr.bfType = (WORD)COLOR_0X4D42; // "BM"
     bmfHdr.bfSize = (DWORD)(sizeof(BitMapFileHeader) + sizeof(BitMapV4Header) + bmiHdr.bV4SizeImage);
-    bmfHdr.bfReserved1 = 0;
-    bmfHdr.bfReserved2 = 0;
+    bmfHdr.bfReserved1 = ZERO;
+    bmfHdr.bfReserved2 = ZERO;
     bmfHdr.bfOffBits = (DWORD)(sizeof(BitMapFileHeader) + sizeof(BitMapV4Header));
 
     // 分配内存
     uint8_t *data = (uint8_t *)malloc(bmfHdr.bfSize);
     if (!data) {
-        UInt8Data empty = {NULL, 0};
+        UInt8Data empty = {NULL, ZERO};
         return empty;
     }
     
@@ -219,10 +241,10 @@ static UInt8Data toBitmapBGRA8888(OH_Drawing_Bitmap *bitmap) {
     // 复制像素数据（注意BMP是从下到上存储的）
     for (int i = 0; i < h; i++) {
         // 从源位图的最后一行开始复制（BMP是从下到上存储的）
-        uint8_t *srcLine = bitmapAddr + (w * (h - i - 1) * 4);
-        safe_memcpy(start, destsz, srcLine, w * 4);
-        start += w * 4;
-        destsz -= w * 4;
+        uint8_t *srcLine = bitmapAddr + (w * (h - i - ONE) * FOUR);
+        safe_memcpy(start, destsz, srcLine, w * FOUR);
+        start += w * FOUR;
+        destsz -= w * FOUR;
     }
     
     UInt8Data u8data;
@@ -238,7 +260,8 @@ static UInt8Data toBitmapBGRA8888(OH_Drawing_Bitmap *bitmap) {
  *
  * @return 图片数组数据
  */
-UInt8Data TeXRenderToBitmap(OH_Drawing_Bitmap *bitmap, OH_Drawing_ColorFormat colorFormat) {
+UInt8Data TeXRenderToBitmap(OH_Drawing_Bitmap *bitmap, OH_Drawing_ColorFormat colorFormat)
+{
     if (colorFormat == COLOR_FORMAT_RGB_565) {
         return toBitmapRGB565(bitmap);
     } else {
@@ -254,7 +277,8 @@ UInt8Data TeXRenderToBitmap(OH_Drawing_Bitmap *bitmap, OH_Drawing_ColorFormat co
  *
  * @return bitmap对象
  */
-OH_Drawing_Bitmap *initGraphics2D(uint32_t w, uint32_t h, OH_Drawing_ColorFormat colorFormat) {
+OH_Drawing_Bitmap *initGraphics2D(uint32_t w, uint32_t h, OH_Drawing_ColorFormat colorFormat)
+{
     OH_Drawing_Bitmap *bitmap = OH_Drawing_BitmapCreate();
     // 定义bitmap的像素格式
     OH_Drawing_BitmapFormat cFormat{colorFormat, ALPHA_FORMAT_PREMUL};
@@ -276,7 +300,8 @@ OH_Drawing_Bitmap *initGraphics2D(uint32_t w, uint32_t h, OH_Drawing_ColorFormat
  * @return 图片数组数据
  */
 UInt8Data drawCircleImage(char *str, float fontSize, uint32_t fontColor, uint32_t fontBackGroupColor,
-                          uint32_t backGroupColor, float textHeight, OH_Drawing_ColorFormat colorFormat) {
+                          uint32_t backGroupColor, float textHeight, OH_Drawing_ColorFormat colorFormat)
+{
     OH_Drawing_Bitmap *bitmap = initGraphics2D(textHeight, textHeight, colorFormat); // 初始化bitmap
     OH_Drawing_Canvas *bitmapCanvas = OH_Drawing_CanvasCreate();                     // 创建一个画布对象
     OH_Drawing_CanvasBind(bitmapCanvas, bitmap); // 将Canvas与bitmap绑定，Canvas绘制的内容会输出到绑定的bitmap内存中
@@ -337,7 +362,8 @@ UInt8Data drawCircleImage(char *str, float fontSize, uint32_t fontColor, uint32_
  */
 UInt8Data drawRectImage(char *str, float fontSize, uint32_t fontColor, uint32_t fontBackGroupColor,
                         uint32_t backGroupColor, float textHeight, OH_Drawing_ColorFormat colorFormat, float padding,
-                        float radius, bool isFakeBoldText) {
+                        float radius, bool isFakeBoldText)
+{
     // 画文本和文本颜色
     OH_Drawing_Font *font = OH_Drawing_FontCreate();                                           // 创建font字体对象
     OH_Drawing_FontSetFakeBoldText(font, isFakeBoldText);                                      // 用于设置增加描边宽度以近似粗体字体效果。
@@ -407,7 +433,8 @@ UInt8Data drawRectToolImage(char *str1, char *str2, float fontSize, uint32_t fon
                             uint32_t backGroupColor, uint32_t borderColor, float borderWidth,
                             uint32_t dividingLineColor, float dividingLineWidth, float textHeight,
                             OH_Drawing_ColorFormat colorFormat, float padding, float lineLeftPadding,
-                            float lineRightPadding) {
+                            float lineRightPadding)
+{
     // 画文本1和文本1颜色
     OH_Drawing_Font *font1 = OH_Drawing_FontCreate();                     // 创建font字体对象
     OH_Drawing_FontSetTextSize(font1, fontSize);                          // 设置字体大小
--- markdown_hybrid/src/main/cpp/utils.h
+++ markdown_hybrid/src/main/cpp/utils.h
@@ -11,10 +11,6 @@
 extern "C" {
 #endif
 
-
-//extern int memset_s(void *dest, uint32_t destsz, int ch, uint32_t count);
-//extern int memcpy_s(void *dest, uint32_t destsz, void *src, uint32_t count);
-
 #define BI_RGB 0x0
 #define BI_BITFIELDS 0x3
 
@@ -52,7 +48,7 @@ typedef struct tagRGBQUAD {
     BYTE rgbGreen;                    // 绿色的亮度(值范围为0-255)
     BYTE rgbRed;                      // 红色的亮度(值范围为0-255)
     BYTE rgbReserved;                 // 保留, 必须为0
-} __attribute__((packed)) RgbQuad;    //RGBQUAD;
+} __attribute__((packed)) RgbQuad;    // RGBQUAD;
 
 #ifdef __cplusplus
 }
