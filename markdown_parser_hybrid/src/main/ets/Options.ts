a1f96ae16c6b55e1eab3bb638072ede5648a7504
yangtao <taoyangdd@isoftstone.com>
2026-06-26 15:03:00 +0800

    feat: markdown options interface

--- markdown_parser_hybrid/src/main/ets/Options.ts
+++ markdown_parser_hybrid/src/main/ets/Options.ts
@@ -3,7 +3,7 @@ import { JsNode } from "./JsNode"
 /*
  * markdown初始化选项
  */
-export class Options {
+export interface Options {
   /*
    * 用于JsNode复用
    */
@@ -12,35 +12,34 @@ export class Options {
    * 是否包含SourceSpan信息 0:不包含(默认) 1:仅Block节点 2:全部节点
    */
   includeSourceSpans: 0 | 1 | 2
-
-  ADPlugin: boolean = false
-  BlockAudioPlugin: boolean = false
-  BlockVideoPlugin: boolean = false
-  CodeListPlugin: boolean = false
-  DescListPlugin: boolean = false
-  EmojiPlugin: boolean = false
-  EmojiPlugin_light: boolean = false
-  FootnotePlugin: boolean = false
-  HighlightDelimeterPlugin: boolean = false
-  HtmlPlugin: boolean = false
-  IdHeadingPlugin: boolean = false
-  ImageCollectPlugin: boolean = false
-  ImageSinglePlugin: boolean = false
-  ImageSlidePlugin: boolean = false
-  ImageStylePlugin: boolean = false
-  ImageTextMixPlugin: boolean = false
-  ImageVideoPlugin: boolean = false
-  LatexMathPluginV2: boolean = false
-  LinkifyPlugin: boolean = false
-  LinkifyPlugin_regs: Array<string> | undefined = undefined
-  LinkViewPlugin: boolean = false
-  NodeIdPlugin: boolean = false
-  StrikethroughPlugin: boolean = false
-  SubPlugin: boolean = false
-  SupPlugin: boolean = false
-  TablePlugin: boolean = false
-  TaskListPlugin: boolean = false
-  TocPlugin: boolean = false
-  WhitelistLooseDelimiterPlugin: boolean = false
-  WhitelistLooseDelimiterPlugin_DelimiterFilterMode: "Permissive" | "Strict" | Array<String> = "Permissive"
+  ADPlugin?: boolean
+  BlockAudioPlugin?: boolean
+  BlockVideoPlugin?: boolean
+  CodeListPlugin?: boolean
+  DescListPlugin?: boolean
+  EmojiPlugin?: boolean
+  EmojiPlugin_light?: boolean
+  FootnotePlugin?: boolean
+  HighlightDelimeterPlugin?: boolean
+  HtmlPlugin?: boolean
+  IdHeadingPlugin?: boolean
+  ImageCollectPlugin?: boolean
+  ImageSinglePlugin?: boolean
+  ImageSlidePlugin?: boolean
+  ImageStylePlugin?: boolean
+  ImageTextMixPlugin?: boolean
+  ImageVideoPlugin?: boolean
+  LatexMathPluginV2?: boolean
+  LinkifyPlugin?: boolean
+  LinkifyPlugin_regs?: Array<string>
+  LinkViewPlugin?: boolean
+  NodeIdPlugin?: boolean
+  StrikethroughPlugin?: boolean
+  SubPlugin?: boolean
+  SupPlugin?: boolean
+  TablePlugin?: boolean
+  TaskListPlugin?: boolean
+  TocPlugin?: boolean
+  WhitelistLooseDelimiterPlugin?: boolean
+  WhitelistLooseDelimiterPlugin_DelimiterFilterMode?: "Permissive" | "Strict" | Array<String>
 }
